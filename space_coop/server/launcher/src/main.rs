2131f472d3e87a5ed9c7f5fc3681d5e230310960
Alex McArther <acmcarther@gmail.com>
2017-01-26 13:47:31 -0800

    Move hotload and server stuff into client_server

--- space_coop/server/launcher/src/main.rs
+++ space_coop/server/launcher/src/main.rs
@@ -1,51 +1,12 @@
 extern crate clap;
-extern crate hotload;
+extern crate launcher;
 
-use clap::{App, Arg, ArgMatches};
-use hotload::BasicProxy;
-use hotload::Hotloader;
-use std::env;
-use std::path::PathBuf;
-use std::str::FromStr;
-use std::thread;
-use std::time::Duration;
+use clap::App;
 
 static EXAMPLE_COMMAND: &'static str = "space_coop -- -d ./core/libcore.so";
 
 fn main() {
-  let matches = App::new("space coop")
-    .usage(EXAMPLE_COMMAND)
-    .arg(Arg::with_name("port")
-      .short("p")
-      .long("port")
-      .default_value("8829")
-      .help("Port to host on")
-      .takes_value(true))
-    .arg(Arg::with_name("dylib_path")
-      .short("d")
-      .long("dylib")
-      .default_value("./bazel-bin/space_coop/server/core/libcore.so")
-      .help("Path to dylib")
-      .takes_value(true))
-    .get_matches();
-
-  // prototype2::server::start(port_from(&server_matches))
-  //let port = port_from(&matches);
-  //println!("prospective port: {}", port_from(&matches));
-  //let core = Core::new_with_port(port);
-  //core.run();
-
-  let dylib_path = dylib_from(&matches);
-  println!("current_dir:{:?}", env::current_dir());
-  println!("dylib_path:{:?}", PathBuf::from(dylib_path.clone()));
-  let mut hotloader = Hotloader::<BasicProxy>::new(PathBuf::from(dylib_path));
-  hotloader.get_proxy().unwrap().set_flags(matches);
-  loop {
-    hotloader.get_proxy().unwrap().run();
-    thread::sleep(Duration::from_millis(200));
-  }
-}
-
-fn dylib_from(matches: &ArgMatches) -> String {
-  matches.value_of("dylib_path").unwrap().to_owned()
+  launcher::run(
+      App::new("space coop").usage(EXAMPLE_COMMAND),
+      "./bazel-bin/space_coop/server/core/libcore.so".to_owned());
 }
