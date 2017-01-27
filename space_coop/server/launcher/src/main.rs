86ffeae63b59f96debac16768d56b6186bfe3d56
Alex McArther <acmcarther@gmail.com>
2017-01-26 23:51:05 -0800

    Remove extra junk, and clean up hotload stuff

--- space_coop/server/launcher/src/main.rs
+++ space_coop/server/launcher/src/main.rs
@@ -1,12 +1,45 @@
 extern crate clap;
-extern crate launcher;
+extern crate hotload;
 
 use clap::App;
+use clap::Arg;
+use clap::ArgMatches;
+use hotload::BasicProxy;
+use hotload::Hotloader;
+use std::env;
+use std::path::PathBuf;
+use std::thread;
+use std::time::Duration;
 
 static EXAMPLE_COMMAND: &'static str = "space_coop -- -d ./core/libcore.so";
 
+fn dylib_from(matches: &ArgMatches) -> String {
+  matches.value_of("dylib_path").unwrap().to_owned()
+}
+
 fn main() {
-  launcher::run(
-      App::new("space coop").usage(EXAMPLE_COMMAND),
-      "./bazel-bin/space_coop/server/core/libcore.so".to_owned());
+  let matches = App::new("space coop server")
+    .usage(EXAMPLE_COMMAND)
+    .arg(Arg::with_name("port")
+      .short("p")
+      .long("port")
+      .default_value("8829")
+      .help("Port to host on")
+      .takes_value(true))
+    .arg(Arg::with_name("dylib_path")
+      .short("d")
+      .long("dylib")
+      .default_value("./bazel-bin/spacecoop/server/core/libcore.so")
+      .help("Path to dylib")
+      .takes_value(true))
+    .get_matches();
+
+  let dylib_path = PathBuf::from(dylib_from(&matches));
+  println!("current_dir:{:?}", env::current_dir());
+  println!("dylib_path:{:?}", dylib_path);
+  let mut hotloader = Hotloader::<BasicProxy>::new(dylib_path, matches);
+  loop {
+    hotloader.get_proxy().unwrap().run();
+    thread::sleep(Duration::from_millis(200));
+  }
 }
